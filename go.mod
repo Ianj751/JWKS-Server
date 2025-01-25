43ecb96bd654c38a2708fad6f8d997e09afb7ebc
Ian Jackson <136002718+Ianj751@users.noreply.github.com>
2025-01-24 22:21:26 -0600

    Generate JWTs, Implemented Auth Endpoints, and most of the jwks endpoint

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/Ianj751
 go 1.23.4
 
 require github.com/google/uuid v1.6.0
+
+require github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
+/* TODO:
+- Write Tests (look into fuzzing)
+- Figure out if the post request will have parameters??
+- Organize into different files
+*/
+
 import (
 	"crypto/rand"
 	"crypto/rsa"
 	"encoding/json"
+	"fmt"
 	"log"
 	"math/big"
 	"net/http"
+
 	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
 )
 
-/*TODO: Create a JWK store */
 type JWK struct{
 	Alg string `json:"alg"` //algorithm intended for use with the key (RS256)
 	Kty string `json:"kty"` //algorithm family used with the key (this will be RSA)
@@ -25,7 +33,10 @@ type JWKS struct{
 	Keys []JWK `json:"keys"`
 }
 
-//
+//global keys variable 
+var keys []JWK
+var jwts []string
+
 func NewJWK(public rsa.PublicKey)JWK{
 	return JWK{
 		"RS256", //alg
@@ -51,15 +62,63 @@ func generateRSAKeys()(*rsa.PrivateKey, rsa.PublicKey){
 
 	return privKey, privKey.PublicKey
 }
+func generateJWT(expired bool)(string, rsa.PublicKey){
+	numTime := 5 * time.Minute
+
+	if expired{
+		numTime = -numTime
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, 
+		jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(numTime)),
+			IssuedAt: jwt.NewNumericDate(time.Now()),
+		})
+	privk, _ := generateRSAKeys()
+	str, err := token.SignedString(privk)
+	if err != nil{
+		log.Fatal(err)
+	}
+	return str, privk.PublicKey
+}
+
 /* A /auth endpoint that returns an unexpired, signed JWT on a POST request.
 If the “expired” query parameter is present, issue a JWT signed with the expired key pair and the expired expiry. */
 func handleAuth(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "application/json")
-	if r.Method != "POST"{
+	if r.Method != http.MethodPost{
 		http.Error(w, "Request method is not Allowed", http.StatusMethodNotAllowed)
 	}
-}
+	encoder := json.NewEncoder(w)
+	encoder.SetIndent("", "  ")
 
+	params := r.URL.Query()
+	if _, ok := params["expired"]; ok{
+		//return an expired jwt with the expired key pair and expired expiry
+		tokenstr, _ := generateJWT(true)
+		encoder.Encode(tokenstr)
+	}else{
+		tokenstr, pubk := generateJWT(false)
+		/* Chat GPT prompt for the below jwt.Parse :
+		how can i use jwt.parse if i used an rsa.privatekey with the signed string */
+		token, err := jwt.Parse(tokenstr, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+				return nil, fmt.Errorf("unexpected signing method")
+			}
+			return pubk, nil
+		})
+		if err != nil{
+			log.Fatal(err)
+		}
+		expTime, _ := token.Claims.GetExpirationTime()
+		data := map[string]interface{}{
+			"jwt": tokenstr,
+			"expiry":  expTime,
+		}
+		encoder.Encode(data)
+		
+	}
+}
 /* A RESTful JWKS endpoint that serves the public keys in JWKS format.
 Only serve keys that have not expired.
  */
@@ -68,14 +127,14 @@ func handleJwks(w http.ResponseWriter, r *http.Request){
 	_, rawKey := generateRSAKeys()
 	key := NewJWK(rawKey)
 
-	var keys []JWK
-
-	if keyIsExpired(key){
+	if !keyIsExpired(key){
 		keys = append(keys, key)
 	}
 	jwks := JWKS{Keys:keys}
 
-	json.NewEncoder(w).Encode(jwks)
+	encoder := json.NewEncoder(w)
+	encoder.SetIndent("", "  ")
+	encoder.Encode(jwks)
 }
 
 
